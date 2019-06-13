8c43dfc18fdcd5abcd22b3992eb8337ec0af007e
Holden Clark and Na Hyung Choi <na@>
2019-06-13 12:01:20 -0700

    rename variables and delete comments

--- js/scripts.js
+++ js/scripts.js
@@ -1,32 +1,41 @@
-var userPhrase = "";
+var userPhrase, formattedInput, encryptedPhrase = "";
 var numLetterOnly = /[^0-9a-z]/gi;
-var encryptedPhrase = "";
-var colSize, regexTemp, userPhraseRowSplit;
+var colSize, rowSize, regexTemp, userPhraseRowSplit;
 var inputArray = [];
 
 
 var encrypt = function(input){
-  encryptedPhrase = (input.replace(numLetterOnly, '')).toLowerCase();     // takes out punctuation, spaces, and uppercase
-  colSize = Math.ceil(Math.sqrt(input.length));
+  formattedInput = (input.replace(numLetterOnly, '')).toLowerCase();     // takes out punctuation, spaces, and uppercase
+  colSize = Math.ceil(Math.sqrt(formattedInput.length));
+  rowSize = colSize;
   regexTemp = new RegExp(".{1," + colSize + "}");
   userPhraseRowSplit = new RegExp(regexTemp.source, regexTemp.flags + "g");     // creates regex to split a word by rows
-  inputArray = input.match(userPhraseRowSplit);         // outputs ["ba", "nd"]
-  // encryptedPhrase = encryptedPhrase.splice(colSize, 0, " ")
+  inputArray = formattedInput.match(userPhraseRowSplit);         // outputs ["ba", "nd"]
+  scramble(inputArray);
+}
 
+var scramble = function(array) {
+  for (col = 0; col < rowSize; col++){
+    for (row = 0; row < array.length; row++){
+      encryptedPhrase += array[row].charAt(col);
+
+    }
+  }
 }
 
 $(function(){
   $("#user-input").submit(function(event){
     event.preventDefault();
 
-    // romanNumeral = "";
+    encryptedPhrase = "";
 
     userPhrase = $("#userInput").val();
 
     encrypt(userPhrase);
 
-    $("#result").empty().append(inputArray);
+    // $("#result").empty().append(inputArray);
     // $("#result").empty().append(rowSize);
+    $("#result").empty().append(encryptedPhrase);
 
   });
 });
