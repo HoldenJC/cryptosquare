7ce368b1b24fc85d1b0ec7b8ddee825383affd43
Holden Clark and Na Hyung Choi <holden@>
2019-06-13 13:18:13 -0700

    add spacing every 5 letters to encrypted phrase

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,6 @@ var numLetterOnly = /[^0-9a-z]/gi;
 var colSize, rowSize, regexTemp, userPhraseRowSplit;
 var inputArray = [];
 
-
 var encrypt = function(input){
   formattedInput = (input.replace(numLetterOnly, '')).toLowerCase();     // takes out punctuation, spaces, and uppercase
   colSize = Math.ceil(Math.sqrt(formattedInput.length));
@@ -12,30 +11,33 @@ var encrypt = function(input){
   userPhraseRowSplit = new RegExp(regexTemp.source, regexTemp.flags + "g");     // creates regex to split a word by rows
   inputArray = formattedInput.match(userPhraseRowSplit);         // outputs ["ba", "nd"]
   scramble(inputArray);
+  spacing(encryptedPhrase);
 }
 
 var scramble = function(array) {
   for (col = 0; col < rowSize; col++){
     for (row = 0; row < array.length; row++){
       encryptedPhrase += array[row].charAt(col);
-
     }
   }
+}
+
+var spacing = function(string) {
+  var spacingArray = string.split("");
+  for (var i = 5; i < spacingArray.length; i += 6){
+    spacingArray.splice(i, 0, " ");
+  }
+  encryptedPhrase = spacingArray.join("");
 }
 
 $(function(){
   $("#user-input").submit(function(event){
     event.preventDefault();
-
     encryptedPhrase = "";
 
     userPhrase = $("#userInput").val();
 
     encrypt(userPhrase);
-
-    // $("#result").empty().append(inputArray);
-    // $("#result").empty().append(rowSize);
     $("#result").empty().append(encryptedPhrase);
-
   });
 });
