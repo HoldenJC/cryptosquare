0802ba2b86563f28f3301d5412f8a964572d3b4f
Holden Clark and Na Hyung Choi <holden@>
2019-06-13 13:41:25 -0700

    consolidate colSize and rowSize variables into squareSide

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,12 @@
 var userPhrase, formattedInput, encryptedPhrase = "";
 var numLetterOnly = /[^0-9a-z]/gi;
-var colSize, rowSize, regexTemp, userPhraseRowSplit;
+var squareSide, regexTemp, userPhraseRowSplit;
 var inputArray = [];
 
 var encrypt = function(input){
   formattedInput = (input.replace(numLetterOnly, '')).toLowerCase();     // takes out punctuation, spaces, and uppercase
-  colSize = Math.ceil(Math.sqrt(formattedInput.length));
-  rowSize = colSize;
-  regexTemp = new RegExp(".{1," + colSize + "}");
+  squareSide = Math.ceil(Math.sqrt(formattedInput.length));
+  regexTemp = new RegExp(".{1," + squareSide + "}");
   userPhraseRowSplit = new RegExp(regexTemp.source, regexTemp.flags + "g");     // creates regex to split a word by rows
   inputArray = formattedInput.match(userPhraseRowSplit);         // outputs ["ba", "nd"]
   scramble(inputArray);
@@ -15,7 +14,7 @@ var encrypt = function(input){
 }
 
 var scramble = function(array) {
-  for (col = 0; col < rowSize; col++){
+  for (col = 0; col < squareSide; col++){
     for (row = 0; row < array.length; row++){
       encryptedPhrase += array[row].charAt(col);
     }
